7b989ec2cea4d0ba4726604209e857bc01a99e5e
Marek Jakubowski <marek.jakubowski@ttpsc.pl>
2023-04-27 08:47:55 +0200

    Add description for privilege type

--- pkg/conjurpolicy/privilege.go
+++ pkg/conjurpolicy/privilege.go
@@ -2,5 +2,6 @@ package conjurpolicy
 
 //go:generate go run github.com/abice/go-enum --marshal
 
+// Privilege is an enum representing Conjur type of transactions that are permitted
 // ENUM(read, execute, update, create)
 type Privilege int
